4d355106f24d4c950f82d04516f2fff71ebfaa64
Vlad Fedorovich <72762763+sskef-web@users.noreply.github.com>
2024-04-26 21:30:22 +0300

    update user page

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-String appTitle = "Achieve Club";
+String appTitle = "User";
 
 void main() {
-    runApp(MyApp());
+    runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
 
     @override
     Widget build(BuildContext context) {
         return Center(
           child: MaterialApp(
-            title: '$appTitle',
+            title: appTitle,
             theme: ThemeData(
-                colorScheme: ColorScheme.fromSeed(seedColor: Color.fromRGBO(
+                colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromRGBO(
                     11, 106, 108, 1.0)),
                     useMaterial3: true,
               ),
               darkTheme: ThemeData(
-                  colorScheme: ColorScheme.fromSeed(seedColor: Color.fromRGBO(
+                  colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromRGBO(
                       11, 106, 108, 1.0), brightness: Brightness.dark),
                   useMaterial3: true,
               ),
-            home: AuthenticationPage(),
+            home: const AuthenticationPage(),
           ),
         );
     }
 }
 
 class AuthenticationPage extends StatefulWidget {
+  const AuthenticationPage({super.key});
+
     @override
     _AuthenticationPageState createState() => _AuthenticationPageState();
 }
@@ -101,7 +105,7 @@ class LoginPage extends StatelessWidget {
     final Function() loginCallback;
     final Function() registerCallback;
 
-    LoginPage({required this.loginCallback, required this.registerCallback});
+    LoginPage({super.key, required this.loginCallback, required this.registerCallback});
 
     final TextEditingController _emailController = TextEditingController();
     final TextEditingController _passwordController = TextEditingController();
@@ -112,72 +116,72 @@ class LoginPage extends StatelessWidget {
     Widget build(BuildContext context) {
         return Scaffold(
             appBar: AppBar(
-                title: Text('$appTitle'),
+                title: Text(appTitle),
                 centerTitle: true,
             ),
             body: Padding(
-                padding: EdgeInsets.all(16.0),
+                padding: const EdgeInsets.all(16.0),
                 child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                         TextField(
                             controller: _emailController,
-                            decoration: InputDecoration(
+                            decoration: const InputDecoration(
                                 labelText: 'Email',
                             ),
                         ),
-                        SizedBox(height: 16.0),
+                        const SizedBox(height: 16.0),
                         TextField(
                             controller: _passwordController,
-                            decoration: InputDecoration(
+                            decoration: const InputDecoration(
                                 labelText: 'Password',
                             ),
                             obscureText: true,
                         ),
-                        SizedBox(height: 24.0),
+                        const SizedBox(height: 24.0),
                         SizedBox(
                             width: 150.0,
                             height: 75.0,
                             child: ElevatedButton(
-                                child: Text('Login', textAlign: TextAlign.center),
                                 onPressed: loginCallback,
+                                child: const Text('Login', textAlign: TextAlign.center),
                             ),
                         ),
-                        SizedBox(height: 16.0),
+                        const SizedBox(height: 16.0),
                         TextButton(
                             onPressed: () {
                                 showDialog(
                                     context: context,
                                     builder: (BuildContext context) {
                                         return AlertDialog(
-                                            title: Text('Register', textAlign: TextAlign.center,),
-                                            content: Container(
+                                            title: const Text('Register', textAlign: TextAlign.center,),
+                                            content: SizedBox(
                                                 width: 500,
                                                 height: 500,
                                                 child: Column(
                                                     children: [
                                                         TextField(
                                                             controller: _registerEmailController,
-                                                            decoration: InputDecoration(
+                                                            decoration: const InputDecoration(
                                                                 labelText: 'Name',
                                                             ),
                                                         ),
                                                         TextField(
                                                             controller: _registerEmailController,
-                                                            decoration: InputDecoration(
+                                                            decoration: const InputDecoration(
                                                                 labelText: 'Surname',
                                                             ),
                                                         ),
                                                         TextField(
                                                             controller: _registerEmailController,
-                                                            decoration: InputDecoration(
+                                                            decoration: const InputDecoration(
                                                                 labelText: 'Email',
                                                             ),
                                                         ),
-                                                        SizedBox(height: 16.0),
+                                                        const SizedBox(height: 16.0),
                                                         TextField(
                                                             controller: _registerPasswordController,
-                                                            decoration: InputDecoration(
+                                                            decoration: const InputDecoration(
                                                                 labelText: 'Password',
                                                             ),
                                                             obscureText: true,
@@ -190,15 +194,15 @@ class LoginPage extends StatelessWidget {
                                                     width: 150.0,
                                                     height: 75.0,
                                                     child: ElevatedButton(
-                                                        child: Text('Registrate', textAlign: TextAlign.center),
                                                         onPressed: registerCallback,
+                                                        child: const Text('Registrate', textAlign: TextAlign.center),
                                                     ),
                                                 ),
                                         ]);
                                     },
                                 );
                             },
-                            child: Text('Registration', textAlign: TextAlign.center),
+                            child: const Text('Registration', textAlign: TextAlign.center),
                         ),
                     ],
                 ),
@@ -210,7 +214,7 @@ class LoginPage extends StatelessWidget {
 class HomePage extends StatefulWidget {
     final Function() logoutCallback;
 
-    HomePage({required this.logoutCallback});
+    const HomePage({super.key, required this.logoutCallback});
 
     @override
     _HomePageState createState() => _HomePageState();
@@ -220,9 +224,9 @@ class _HomePageState extends State<HomePage> {
     int _currentIndex = 0;
 
     final List<Widget> _tabs = [
-        Tab1Page(),
-        Tab2Page(),
-        Tab3Page(),
+        const Tab1Page(),
+        const Tab2Page(),
+        const Tab3Page(),
     ];
 
     @override
@@ -268,31 +272,139 @@ class _HomePageState extends State<HomePage> {
             ),
             floatingActionButton: FloatingActionButton(
                 onPressed: widget.logoutCallback,
-                child: Icon(Icons.logout),
+                child: const Icon(Icons.logout),
             ),
         );
     }
 }
 
 class Tab1Page extends StatelessWidget {
+  const Tab1Page({super.key});
+
     @override
     Widget build(BuildContext context) {
-        return Center (
-            child: ClipRRect(
-            borderRadius: const BorderRadius.all(Radius.circular(500.0)),
-                child: Image.network("https://scontent-fra3-2.xx.fbcdn.net/v/t39.30808-6/351172401_580626204259601_4702578806292022547_n.jpg?_nc_cat=1&ccb=1-7&_nc_sid=5f2048&_nc_ohc=y0Tz4XfPWqUAb5vSiGw&_nc_ht=scontent-fra3-2.xx&oh=00_AfA8xurik-zP7srJY1FEY9mvVABy5nN35NZ9ypvGJOrnUg&oe=6631C943",
-                height: 300.0,
-                width: 300.0,
+        return Scaffold(
+            body: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                        const CircleAvatar(
+                            radius: 80.0,
+                            backgroundImage: AssetImage(''),
+                        ),
+                        const SizedBox(height: 16.0),
+                        const Text(
+                            'Имя Фамилия',
+                            style: TextStyle(
+                                fontSize: 24.0,
+                                fontWeight: FontWeight.bold,
+                            ),
+                        ),
+                        const SizedBox(height: 16.0),
+                        Container(
+                            decoration: BoxDecoration(
+                                color: Theme.of(context).brightness == Brightness.dark ? Colors.grey[800] : Colors.grey[200],
+                                borderRadius: BorderRadius.circular(8.0),
+                            ),
+                            padding: const EdgeInsets.all(16.0),
+                            child: const Column(
+                                children: [
+                                    Text(
+                                        'Выполнено достижений: 10',
+                                        style: TextStyle(
+                                            fontSize: 18.0,
+                                        ),
+                                    ),
+                                    SizedBox(height: 8.0),
+                                    Text(
+                                        'Процент выполненных достижений: 80%',
+                                        style: TextStyle(
+                                            fontSize: 18.0,
+                                        ),
+                                    ),
+                                ],
+                            ),
+                        ),
+                        const SizedBox(height: 24.0),
+                        const Text(
+                            'Достижения:',
+                            style: TextStyle(
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold,
+                            ),
+                        ),
+                        const SizedBox(height: 8.0),
+                        Expanded(
+                            child: ListView(
+                                children: const [
+                                    AchievementItem(
+                                        icon: Icons.star,
+                                        title: 'Достижение 1',
+                                        description: 'Описание достижения 1',
+                                        xp: 100,
+                                        completionPercentage: 50,
+                                    ),
+                                    AchievementItem(
+                                        icon: Icons.star,
+                                        title: 'Достижение 2',
+                                        description: 'Описание достижения 2',
+                                        xp: 200,
+                                        completionPercentage: 70,
+                                    ),
+                                    AchievementItem(
+                                        icon: Icons.star,
+                                        title: 'Достижение 3',
+                                        description: 'Описание достижения 3',
+                                        xp: 300,
+                                        completionPercentage: 90,
+                                    ),
+                                ],
+                            ),
+                        ),
+                    ],
                 ),
-        ),
+            ),
+        );
+    }
+}
+
+class AchievementItem extends StatelessWidget {
+    final IconData icon;
+    final String title;
+    final String description;
+    final int xp;
+    final int completionPercentage;
+
+    const AchievementItem({super.key, required this.icon, required this.title, required this.description, required this.xp, required this.completionPercentage});
+
+    @override
+    Widget build(BuildContext context) {
+        return Card(
+            child: ListTile(
+                leading: Icon(icon),
+                title: Text(title),
+                subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                        Text(description),
+                        const SizedBox(height: 4.0),
+                        Text('XP: $xp'),
+                        const SizedBox(height: 4.0),
+                        Text('Процент выполнения: $completionPercentage%'),
+                    ],
+                ),
+            ),
         );
     }
 }
 
 class Tab2Page extends StatelessWidget {
+  const Tab2Page({super.key});
+
     @override
     Widget build(BuildContext context) {
-        return Center(
+        return const Center(
             child:
             Text('Top 100 users'),
         );
@@ -300,9 +412,11 @@ class Tab2Page extends StatelessWidget {
 }
 
 class Tab3Page extends StatelessWidget {
+  const Tab3Page({super.key});
+
     @override
     Widget build(BuildContext context) {
-        return Center(
+        return const Center(
             child: Text('Top clubs'),
         );
     }
