3948090eddf47442b98b7efa4870674fdc89ce7f
Vlad Fedorovich <72762763+sskef-web@users.noreply.github.com>
2025-01-31 15:55:25 +0300

    Fix moving bottomsheet along with keyboard

--- lib/pages/settingsPage.dart
+++ lib/pages/settingsPage.dart
@@ -117,84 +117,89 @@ class _SettingsPageState extends State<SettingsPage> {
                 onTap: () {
                   showModalBottomSheet(
                     context: context,
+                    isScrollControlled: true,
                     builder: (BuildContext context) {
-                      return Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.only(topLeft: Radius.circular(30), topRight: Radius.circular(30)),
-                          color: Color.fromRGBO(38, 38, 38, 1),
-                        ),
-                        height: 230,
-                        child: Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Row (
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Container(
-                                    margin: EdgeInsets.only(left: 11),
-                                    child: Text(
-                                      textAlign: TextAlign.left,
-                                      'Имя Фамилия',
-                                      style: TextStyle(
-                                          color: Colors.white,
-                                          fontWeight: FontWeight.w900,
-                                          fontSize: 18
+                      return Padding(
+                        padding: EdgeInsets.only(
+                            bottom: MediaQuery.of(context).viewInsets.bottom),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.only(topLeft: Radius.circular(30), topRight: Radius.circular(30)),
+                            color: Color.fromRGBO(38, 38, 38, 1),
+                          ),
+                          height: 240,
+                          child: Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row (
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Container(
+                                      margin: EdgeInsets.only(left: 11),
+                                      child: Text(
+                                        textAlign: TextAlign.left,
+                                        'Имя Фамилия',
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.w900,
+                                            fontSize: 18
+                                        ),
                                       ),
                                     ),
-                                  ),
-                                  IconButton(
-                                    style: IconButton.styleFrom(
-                                      backgroundColor: Color.fromRGBO(91, 91, 91, 1)
+                                    IconButton(
+                                      style: IconButton.styleFrom(
+                                          backgroundColor: Color.fromRGBO(91, 91, 91, 1)
+                                      ),
+                                      icon: Icon(Icons.close, color: Colors.white),
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                      },
                                     ),
-                                    icon: Icon(Icons.close, color: Colors.white),
-                                    onPressed: () {
-                                      Navigator.pop(context);
-                                    },
-                                  ),
-                                ],
-                              ),
-                              SizedBox(height: 16,),
-                              TextField(
-                                decoration: InputDecoration(
-                                  hintText: 'Имя Фамилия',
-                                  hintStyle: TextStyle(color: Colors.white),
-                                  enabledBorder: OutlineInputBorder(
-                                    borderSide: BorderSide(color: Color.fromRGBO(245, 110, 15, 1)),
-                                    borderRadius: BorderRadius.circular(15),
-                                  ),
-                                  focusedBorder: OutlineInputBorder(
-                                    borderSide: BorderSide(color: Color.fromRGBO(245, 110, 15, 1)),
-                                    borderRadius: BorderRadius.circular(15),
-                                  ),
+                                  ],
                                 ),
-                                style: TextStyle(color: Colors.white),
-                              ),
-                              SizedBox(height: 20),
-                              Container(
-                                width: double.infinity,
-                                child: ElevatedButton(
-                                  onPressed: () {
-                                    Navigator.pop(context);
-                                  },
-                                  style: ElevatedButton.styleFrom(
-                                    foregroundColor: Colors.white,
-                                    backgroundColor: Color.fromRGBO(245, 110, 15, 1),
-                                    padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
-                                    textStyle: TextStyle(fontSize: 18),
-                                    shape: RoundedRectangleBorder(
+                                SizedBox(height: 16,),
+                                TextField(
+                                  decoration: InputDecoration(
+                                    hintText: 'Имя Фамилия',
+                                    hintStyle: TextStyle(color: Colors.white),
+                                    enabledBorder: OutlineInputBorder(
+                                      borderSide: BorderSide(color: Color.fromRGBO(245, 110, 15, 1)),
+                                      borderRadius: BorderRadius.circular(15),
+                                    ),
+                                    focusedBorder: OutlineInputBorder(
+                                      borderSide: BorderSide(color: Color.fromRGBO(245, 110, 15, 1)),
                                       borderRadius: BorderRadius.circular(15),
                                     ),
                                   ),
-                                  child: Text(
-                                    'Сохранить',
-                                    style: TextStyle(color: Colors.white,  fontWeight: FontWeight.w500),
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                                SizedBox(height: 20),
+                                Container(
+                                  width: double.infinity,
+                                  child: ElevatedButton(
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                    },
+                                    style: ElevatedButton.styleFrom(
+                                      foregroundColor: Colors.white,
+                                      backgroundColor: Color.fromRGBO(245, 110, 15, 1),
+                                      padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+                                      textStyle: TextStyle(fontSize: 18),
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(15),
+                                      ),
+                                    ),
+                                    child: Text(
+                                      'Сохранить',
+                                      style: TextStyle(color: Colors.white,  fontWeight: FontWeight.w500),
+                                    ),
                                   ),
                                 ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
                         ),
                       );
