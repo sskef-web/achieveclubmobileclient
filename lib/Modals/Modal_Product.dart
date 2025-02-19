455fd4d4f922604e49b0e67480c41019e8b01f3e
Vlad Fedorovich <72762763+sskef-web@users.noreply.github.com>
2025-02-20 00:02:25 +0300

    Moving a folder with modal windows

--- lib/Modals/Modal_Product.dart
+++ lib/Modals/Modal_Product.dart
@@ -1,12 +1,12 @@
 import 'dart:io';
 
-import 'package:achieveclubmobileclient/tabItems/shopTabs/Modals/Modal_Buy_Result.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert';
 
 import '../../../main.dart';
+import 'Modal_Buy_Result.dart';
 
 class ProductModal extends StatelessWidget {
   final int id;
