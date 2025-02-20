0f4643b58084cedd174767fa289a58772717022d
Vlad Fedorovich <72762763+sskef-web@users.noreply.github.com>
2025-02-20 16:24:37 +0300

    Update kt & ndk version

---
+++ android/app/.cxx/RelWithDebInfo/2b2v3y6b/armeabi-v7a/CMakeFiles/3.22.1-g37088a8/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Linux-6.12.10-arch1-1")
+set(CMAKE_HOST_SYSTEM_NAME "Linux")
+set(CMAKE_HOST_SYSTEM_VERSION "6.12.10-arch1-1")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
+
+include("/home/sskef/Android/Sdk/ndk/27.2.12479018/build/cmake/android.toolchain.cmake")
+
+set(CMAKE_SYSTEM "Android-1")
+set(CMAKE_SYSTEM_NAME "Android")
+set(CMAKE_SYSTEM_VERSION "1")
+set(CMAKE_SYSTEM_PROCESSOR "armv7-a")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
